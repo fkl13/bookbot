d8fcd13f116218e70e7027674c64e82289c87040
Fabian Klaiber <fklaiber@posteo.net>
2024-08-03 16:12:37 +0200

    Count characters

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     file_contents = read_file(book_path)
     num_words = count_words(file_contents)
     print("{num_words} words found in the document")
+    num_chars = count_characters(file_contents)
+    print(num_chars)
 
 def read_file(path):
     with open(path) as f:
@@ -11,5 +13,15 @@ def read_file(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    map = {}
+    for c in text:
+        lowered = c.lower()
+        if c in map:
+            map[c] += 1
+        else:
+            map[c]=1
+    return map
 
 main()
